682ab32d3b80730940ba2fecef568637c25477b8
piotr04j <darricck04@gmail.com>
2020-04-05 16:32:46 +0200

    created user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,19 @@
+import { exhaustMap, map, take, tap } from 'rxjs/operators'
+import { Recipe } from './recipe.model'
+import { HttpParams } from '@angular/common/http'
+
+export class User {
+  constructor (
+    public email: string,
+    public id: string,
+    private _token: string,
+    private _tokenExpirationDate: Date
+  ) {}
+
+  get token() {
+    if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
+      return null
+    }
+    return this._token
+  }
+}
